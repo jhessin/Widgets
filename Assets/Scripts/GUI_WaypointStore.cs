19c03d653fb3d21c27dd86e2a8ee73e6e64c8bf9
Jim Hessin <grillbrick@gmail.com>
2015-11-15 23:53:14 -0700

    - Added the laser to the item store

--- Assets/Scripts/GUI_WaypointStore.cs
+++ Assets/Scripts/GUI_WaypointStore.cs
@@ -1,23 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 
 //GUI_WaypointStore : handles the interface for the store transactions
 //shows the available items for purchase, allows the player to sell his own inventory, and buy transforms
+
 namespace GrillbrickStudios
 {
 	public class GUI_WaypointStore : MonoBehaviour
 	{
-		public Texture2D storeBG;
 		public GUISkin customSkin;
+		public Texture2D laserTile;
+		private bool openStore;
 
 		private Widget_Inventory playerInventory;
-		private bool openStore = false;
+		private StoreFront store;
+		public Texture2D storeBG;
 
 		//------------------------------------------------------------
 		public void Awake()
 		{
 			playerInventory = FindObjectOfType<Widget_Inventory>();
+			store = FindObjectOfType<StoreFront>();
 			if (!playerInventory)
 				Debug.Log("No link to player's inventory.");
 		}
@@ -29,13 +31,30 @@ namespace GrillbrickStudios
 
 			if (!openStore) return;
 
-			GUI.Box(new Rect(0,0,Screen.width, Screen.height), " ");
-			GUI.Label(new Rect(Screen.width/2 - storeBG.width/2, Screen.height/2 - storeBG.height/2, storeBG.width, storeBG.height), storeBG);
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), " ");
+			GUI.Label(
+				new Rect(Screen.width/2 - storeBG.width/2, Screen.height/2 - storeBG.height/2, storeBG.width, storeBG.height),
+				storeBG);
+
+			if (store.CheckTransformationBuy(StoreFront.ItemLaser))
+				if (
+					GUI.Button(
+						new Rect(Screen.width/2 - laserTile.width/2,
+						Screen.height/2 - laserTile.height/2,
+						laserTile.width/2f, laserTile.height/2f), laserTile))
+				{
+					BuyLaser();
+				}
 
 			if (GUI.Button(new Rect(Screen.width/2 - 126, Screen.height - 100, 252, 113), "Close Store"))
 			{
 				StoreFrontToggle();
 			}
+		}
+
+		private void BuyLaser()
+		{
+			store.BuyTransformation(StoreFront.ItemLaser);
 		}
 
 		public void StoreFrontToggle()
--- Assets/Scripts/StoreFront.cs
+++ Assets/Scripts/StoreFront.cs
@@ -1,14 +1,98 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 //StoreFront: handles purchasing of transformations at waypoints
+
 namespace GrillbrickStudios
 {
-	public class StoreFront : MonoBehaviour {
+	public class StoreFront : MonoBehaviour
+	{
 		//This will be called from the store front if CheckTransformationBuy returns true on selecttion
-		public void BuyTransformation()
+
+		public const int ItemShield = 0;
+		public const int ItemHook = 1;
+		public const int ItemFlight = 2;
+		public const int ItemLaser = 3;
+		private ItemCost _costFlight = new ItemCost(1, 4, 4);
+		private ItemCost _costHook = new ItemCost(1, 2, 2);
+		private ItemCost _costLaser = new ItemCost(0, 0, 0);
+
+		private ItemCost _costShield = new ItemCost(1, 1, 1);
+		private GameObject _laser;
+		private GameObject _taser;
+		private Widget_Inventory _inventory;
+
+		public void Start()
 		{
-			
+			GameObject widgetPlayer = GameObject.FindWithTag("Player");
+			_inventory = widgetPlayer.GetComponent<Widget_Inventory>();
+			_laser = GameObject.FindGameObjectWithTag("Laser");
+			_taser = GameObject.FindGameObjectWithTag("Taser");
+
+			DisableAll();
+			EnableTaser();
+		}
+
+		private void EnableTaser()
+		{
+			foreach (EllipsoidParticleEmitter emitter in _taser.GetComponentsInChildren<EllipsoidParticleEmitter>())
+			{
+				emitter.enabled = true;
+			}
+		}
+
+		public void BuyTransformation(int selection)
+		{
+			if (CheckTransformationBuy(selection))
+			{
+				switch (selection)
+				{
+					case ItemShield: //shield
+						_costShield.BuyFrom(_inventory);
+						// TODO: activate shield
+						break;
+					case ItemHook: //hook
+						_costHook.BuyFrom(_inventory);
+						// TODO: activate hook
+						break;
+					case ItemFlight: //flight
+						_costFlight.BuyFrom(_inventory);
+						// TODO: activate flight
+						break;
+					case ItemLaser:
+						_costLaser.BuyFrom(_inventory);
+						DisableAll();
+						EnableLaser();
+						break;
+				}
+			}
+		}
+
+		private void EnableLaser()
+		{
+			foreach (var mesh in _laser.GetComponentsInChildren<MeshRenderer>())
+			{
+				mesh.enabled = true;
+			}
+			foreach (var laserScript in _laser.GetComponentsInChildren<LaserScript>())
+			{
+				laserScript.enabled = true;
+			}
+		}
+
+		private void DisableAll()
+		{
+			foreach (EllipsoidParticleEmitter emitter in _taser.GetComponentsInChildren<EllipsoidParticleEmitter>())
+			{
+				emitter.enabled = false;
+			}
+			foreach (var mesh in _laser.GetComponentsInChildren<MeshRenderer>())
+			{
+				mesh.enabled = false;
+			}
+			foreach (var laserScript in _laser.GetComponentsInChildren<LaserScript>())
+			{
+				laserScript.enabled = false;
+			}
 		}
 
 		/* Ingrediants required for Widget's Transforms----------------------------------------------------------------------------------------------------------------------------------------
@@ -20,42 +104,64 @@ Flight Transform:  boss_windblade, 4 screws, 4 nuts
 		//called from the Store Purchase Button.  Player must select which upgrade they want, which is passed to this 
 		public bool CheckTransformationBuy(int selection)
 		{
-			Widget_Inventory inventory;
-			GameObject widgetPlayer = GameObject.FindWithTag("Player");
 
-			inventory = widgetPlayer.GetComponent<Widget_Inventory>();
 
 			// number of items required to buy for Transformations
-			int bossitem = 0;
-			int screws = 0;
-			int nuts = 0;
+			var bossitem = 0;
+			var screws = 0;
+			var nuts = 0;
 
 			switch (selection)
 			{
-				case 0:	//shield
-					bossitem = 1;
-					screws = 1;
-					nuts = 1;
+				case ItemShield: //shield
+					_costShield.getCost(out bossitem, out screws, out nuts);
 					break;
-				case 1: //hook
-					bossitem = 1;
-					screws = 2;
-					nuts = 2;
+				case ItemHook: //hook
+					_costHook.getCost(out bossitem, out screws, out nuts);
 					break;
-				case 2: //flight
-					bossitem = 1;
-					screws = 4;
-					nuts = 4;
+				case ItemFlight: //flight
+					_costFlight.getCost(out bossitem, out screws, out nuts);
+					break;
+				case ItemLaser:
+					_costLaser.getCost(out bossitem, out screws, out nuts);
 					break;
 				default:
 					return false;
 			}
 
-			if (inventory.CompareItemCount(InventoryItem.BOSS_TRAY, bossitem) &&
-			    inventory.CompareItemCount(InventoryItem.SCREW, screws) &&
-			    inventory.CompareItemCount(InventoryItem.NUT, nuts))
+			if (_inventory.CompareItemCount(InventoryItem.BOSS_TRAY, bossitem) &&
+			    _inventory.CompareItemCount(InventoryItem.SCREW, screws) &&
+			    _inventory.CompareItemCount(InventoryItem.NUT, nuts))
 				return true;
 			return false;
+		}
+
+		private struct ItemCost
+		{
+			public readonly int bossitem;
+			public readonly int screws;
+			public readonly int nuts;
+
+			public ItemCost(int bi, int s, int n)
+			{
+				bossitem = bi;
+				screws = s;
+				nuts = n;
+			}
+
+			public void getCost(out int bi, out int s, out int n)
+			{
+				bi = bossitem;
+				s = screws;
+				n = nuts;
+			}
+
+			public void BuyFrom(Widget_Inventory inventory)
+			{
+				inventory.UseItem(InventoryItem.BOSS_TRAY, bossitem);
+				inventory.UseItem(InventoryItem.SCREW, screws);
+				inventory.UseItem(InventoryItem.NUT, nuts);
+			}
 		}
 	}
 }
